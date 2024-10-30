8b2fc61daa1d453a1f21361b574ba974f6340c80
Rasheek Mahmud <rasheekmah@gmail.com>
2024-10-30 14:08:34 -0400

    Adding author (final maybe)

--- src/app/authors/author/author.component.ts
+++ src/app/authors/author/author.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { AuthorsService } from '../service/authors.service';
+import { Author } from 'src/app/books/model/book';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from "rxjs";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-author',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './author.component.html',
   styleUrl: './author.component.css'
 })
-export class AuthorComponent {
+export class AuthorComponent implements OnInit, OnDestroy {
+  selectedAuthor!: Author | null;
+  private subscription!: Subscription;
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private authorsService: AuthorsService = inject(AuthorsService);
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.subscription = this.authorsService.getAuthorById(id).subscribe({
+        next: (data: Author) => {
+          this.selectedAuthor = data;
+        },
+        error: (_: any) => {
+          this.selectedAuthor = null;
+        }
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
